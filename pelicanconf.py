a400cf0a896a52394c2e612d8754fd18d884cefb
parthiruth <msparthiruth@gmail.com>
2026-04-14 18:55:47 +0530

    github blog added

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,9 +1,9 @@
-AUTHOR = 'Raja CSP Raman'
-SITENAME = 'RajaCSP'
-SITETITLE = 'RajaCSP'
+AUTHOR = 'Parthiban'
+SITENAME = 'Parthiban'
+SITETITLE = 'Parthiban'
 SITEURL = ""
 PATH = "content"
-TIMEZONE = 'America/Moncton'
+TIMEZONE = 'Asia/kolkata'
 DEFAULT_LANG = 'en'
 THEME = 'theme'
 
@@ -12,7 +12,7 @@ DISABLE_URL_HASH = True
 
 # Copyright
 COPYRIGHT_YEAR = 2026
-COPYRIGHT_NAME = 'Raja CSP Raman'
+COPYRIGHT_NAME = 'Parthiban'
 
 # Dark mode support
 THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
@@ -24,7 +24,7 @@ PYGMENTS_STYLE_DARK = 'native'  # Dark mode
 
 # Static files
 STATIC_PATHS = ['images']
-SITELOGO = '/images/csp.jpeg'
+SITELOGO = '/images/Professional-Photo.png'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -38,10 +38,10 @@ DISPLAY_PAGES_ON_MENU = True
 
 # Social widget
 SOCIAL = (
-    ("github", "https://github.com/rajacsp"),
-    ("linkedin", "https://www.linkedin.com/in/raja-csp-raman/"),
+    ("github", "https://github.com/parthiruth"),
+    ("linkedin", "https://www.linkedin.com/in/parthiban-m-407491147"),
     ("substack", "https://articlevil.substack.com/"),
-    ("python", "https://rajacsp.github.io/mlnotes/"),
+    ("python", "https://parthiruth.github.io/pynotes/"),
 )
 
 DEFAULT_PAGINATION = 10
